8d847f7574f831b072b8af16db582603178d63e0
Igor Zhylin <i.a.zhylin@gmail.com>
2020-01-18 20:34:25 +0200

    Add transformations to class

--- XMLtoHTML/Program.cs
+++ XMLtoHTML/Program.cs
@@ -1,12 +1,27 @@
 ﻿namespace XMLtoHTML
 {
     using System;
+    using System.IO;
 
     class Program
     {
-        static void Main(string[] args)
+        static int Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            try
+            {
+                string template = File.ReadAllText(@"Template.xslt");
+
+                string InputXMLFile = " ";
+                string HTMLpath = " ";
+                string OutputHTML = XSLConverter.TransformXMLToHTML(InputXMLFile, template);
+
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.StackTrace.ToString());
+                return -1;
+            }
+            return 0;
         }
 
     }
--- XMLtoHTML/XSLConverter.cs
+++ XMLtoHTML/XSLConverter.cs
@@ -4,8 +4,53 @@
     using System.Xml;
     using System.Xml.Xsl;
 
-    public static class XSLConverter
+    public class XSLConverter
     {
+        private string _XMLPath;
+        private string _XML;
+        private string _Template;
+
+        public XSLConverter(string xmlPath, string template)
+        {
+            this._XMLPath = xmlPath;
+            this._Template = template;
+
+            _XML = !string.IsNullOrEmpty(_XMLPath) ? ReadAllXML() : throw new System.Exception("EMPTY XML FILEPATH!");
+        }
+
+        /// <summary>
+        /// Read Input file from path 
+        /// </summary>
+        /// <returns>Returns HTML page</returns>
+        private string ReadAllXML()
+            => File.ReadAllText(_XMLPath);
+
+        /// <summary>
+        /// Transform file from params from class constructor
+        /// </summary>
+        /// <returns></returns>
+        public string TransformXMLToHTML()
+        {
+            XslCompiledTransform transform = new XslCompiledTransform();
+            using (XmlReader reader = XmlReader.Create(new StringReader(_Template)))
+            {
+                transform.Load(reader);
+            }
+            StringWriter results = new StringWriter();
+            using (XmlReader reader = XmlReader.Create(new StringReader(_XML)))
+            {
+                transform.Transform(reader, null, results);
+            }
+            return results.ToString();
+        }
+
+
+        /// <summary>
+        /// Static Transormer for xml 
+        /// </summary>
+        /// <param name="inputXml">Input string XML</param>
+        /// <param name="xsltString">Template</param>
+        /// <returns>Returns HTML page</returns>
         public static string TransformXMLToHTML(string inputXml, string xsltString)
         {
             XslCompiledTransform transform = new XslCompiledTransform();
